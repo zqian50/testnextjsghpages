ab52f89f678dcb295aa46a46443c4729a1702504
axelKeizoStahl <axelkeizo@gmail.com>
2024-03-03 10:25:40 -0500

    workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/testnextjsghpages",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
