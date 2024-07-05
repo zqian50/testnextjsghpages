b89667ba66f0132aff908614703eca09da14c933
Samuel Sunko <65315023+sam1sun@users.noreply.github.com>
2024-07-05 10:45:10 -0400

    Update page.js

--- app/SBCinfographic/page.js
+++ app/SBCinfographic/page.js
@@ -3,7 +3,7 @@
 import styles from './page.module.css';
 import '../globals.css';
 
-export default function CompetitionPage() {
+export default function SBCinfographic() {
   return (
     <div>
         <embed src="SBCinfographic.pdf" width="800px" />
