0928290a551a372d672f95225e2a714944f89db5
Samuel Sunko <65315023+sam1sun@users.noreply.github.com>
2024-07-05 11:26:18 -0400

    Update page.js

--- app/SBCinfographic/page.js
+++ app/SBCinfographic/page.js
@@ -8,7 +8,7 @@ export default function SBCinfographic() {
   return (
     <div>
     <div className={styles.infographicPDF}>
-        <embed src="SBCinfographic.pdf" width="600vw" height="100vh" />
+        <embed src="SBCinfographic.pdf" width="1000vw" height="1000vh" />
     </div>
     <div className={styles.manybrs}>
     <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
