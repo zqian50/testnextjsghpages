895546188795b54136992cc254b70f2681d2cbdf
Samuel Sunko <65315023+sam1sun@users.noreply.github.com>
2024-07-05 10:57:36 -0400

    Update page.js

--- app/SBCinfographic/page.js
+++ app/SBCinfographic/page.js
@@ -7,7 +7,7 @@ import '../globals.css';
 export default function SBCinfographic() {
   return (
     <div className={styles.infographicPDF}>
-        <embed src="SBCinfographic.pdf" width="1000px" />
+        <embed src="SBCinfographic.pdf" width="1000px" height="1500px" />
     </div>
   );
 }
