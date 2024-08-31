60268f343d0080dd4784a9aa07e122e41bad7d65
axelKeizoStahl <axelkeizo@gmail.com>
2024-08-31 12:15:30 -0400

    remove bloat

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-
-    return config;
-  },
 };
 
 export default nextConfig;
